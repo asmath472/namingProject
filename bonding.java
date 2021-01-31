c6cd2efdcbe08a9677621cfeae89777385ac2fbf
AN_SEONHO <asmath472@gmail.com>
2021-02-01 02:20:56 +0900

    원소 클래스 만들었고 프로젝트 구상했음.

--- bonding.java
+++ bonding.java
@@ -1,10 +1,10 @@
 package _003_java.nazuke;
 
 public class bonding {
-    protected int bondDegree;
-    protected int counterNumber;//상대쪽의 
+    public int bondDegree;
+    public int counterNumber;//상대쪽의 
     
-    public bonding(int bond,int num)
+    public bonding(int num,int bond) //bond 차수가 뒤쪽에 위치한다. 바꿀까?
     {
         this.bondDegree=bond;
         this.counterNumber=num;
--- element.java
+++ element.java
@@ -1,41 +1,43 @@
 package _003_java.nazuke;
 
 public class element {
-    protected String element_sign;//수소, 탄소, 산소 등의 이름을 입력하는 칸.
-    protected int BONDING_MAX;
+    protected String element_sign;//수소, 탄소, 산소 등의 기호를 입력하는 칸.
+    protected int BONDING_MAX; //최대 결합수
     public static int uniqueNumber=0; //식별번호를 넣어둔다.
     protected bonding[] bondingArray=new bonding[4];//bonding 에 대한 정보를 넣어두는 칸
     protected int bonding_index = 0; //bonding array 에 넣을 index
 
-    public element(String element_sign_input)
+    public element(String element_sign_input) //element 의 생성자
     {    
         this.element_sign=element_sign_input; 
         //element sign 생성자. sign 은 반드시 소문자로만 입력하여야 한다.
 
-        this.BONDING_MAX=bondingSet(element_sign);
+        this.BONDING_MAX=bondingNumSet(element_sign);
         //가능한 bonding 수 set
         this.uniqueNumber++; //고유 번호를 부여한다. 0부터..
     }
 
-    protected int bondingSet(String element_sign)
+
+
+    protected int bondingNumSet(String element_sign)
     {
-        if(element_sign=="c"){
+        if(element_sign.equals("c")){
             System.out.println("This is carbon.");
             return 4;
         }
-        else if(element_sign=="o"){
+        else if(element_sign.equals("o")){
             System.out.println("This is Oxygen");
             return 2;
         }
-        else if (element_sign=="h"){
+        else if (element_sign.equals("h")){
             System.out.println("This is Hydrogen");
             return 1;
         }
-        else if (element_sign=="n"){
+        else if (element_sign.equals("n")){
             System.out.println("This is Nitrogen");
             return 3;
         }
-        else if(element_sign=="cl"){
+        else if(element_sign.equals("cl")){
             System.out.println("This is cloride");
         }
         else{
@@ -44,6 +46,45 @@ public class element {
         }
         return 0;
     }
+
+    public int getUniqueNumber(){
+        return this.uniqueNumber;
+    }
+
+    public void bondingEstablish(element object,int bondDegree)
+    {   //단방향 결합만 만듬
+        int objectUniqueNumber=object.getUniqueNumber();
+        bonding newBond = new bonding(objectUniqueNumber,bondDegree);
+        if(this.bonding_index + bondDegree <= this.BONDING_MAX)//새로이 bonding 을 만들 수 있는 경우
+        {
+            for (int i=0;i<bondDegree;i++)
+            {
+                this.bondingArray[bonding_index]=newBond;
+                this.bonding_index++;
+            }
+        }
+        else 
+        {
+            System.out.println("결합 공간이 부족합니다.");
+        }
+    }
+    /*
+    public void bondingDeletation(element object)
+    {
+        int objectUniqueNumber=getUniqueNumber();
+        for (int i=0;i<this.BONDING_MAX;i++)
+        {
+            //try 를 이용해서 바꿀 수 있을 듯 하다.
+            if(this.bondingArray[i].counterNumber==objectUniqueNumber) //찾는 그것이 있다면
+            {
+
+            }
+        }
+    }
+    */ // 이 부분은 나중에 구현하자. 당장의 실행에는 문제가 없을 듯 하다.
+
+    
+
 
 
 }
