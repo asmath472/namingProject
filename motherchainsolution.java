837fbce24982282ec2c3e2419118d7e218ca71e2
AN_SEONHO <asmath472@gmail.com>
2021-02-11 22:15:58 +0900

    설전 작업

---
+++ motherchainsolution.java
@@ -0,0 +1,11 @@
+package _003_java.nazuke;
+
+public class motherchainsolution {
+    public String motherName;
+    public int number;
+    public motherchainsolution(String name, int num)
+    {
+        this.motherName=name;
+        this.number=num;
+    }
+}
--- refinetext.java
+++ refinetext.java
@@ -3,10 +3,15 @@ package _003_java.nazuke;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.lang.model.util.ElementScanner14;
+
 public class refinetext {
     private String compoundName;
     private ArrayList<String> suffixList = new ArrayList<String>();
     private ArrayList<String> prefixList = new ArrayList<String>();
+    private ArrayList<motherchainsolution> motherChainList = new ArrayList<motherchainsolution>();
+    final String nosuffix = "_NO_SUFFIX_";
+
 
     private void getName()
     {
@@ -15,42 +20,105 @@ public class refinetext {
         this.compoundName=sc.next();
     }
 
-    private void importSuffix()
+    private void importMotherChain()
     {
+        this.motherChainList.add(new motherchainsolution("meth", 1));
+        this.motherChainList.add(new motherchainsolution("methan", 1));
+        this.motherChainList.add(new motherchainsolution("eth", 2));
+        this.motherChainList.add(new motherchainsolution("ethan", 2));
+        this.motherChainList.add(new motherchainsolution("prop", 3));
+        this.motherChainList.add(new motherchainsolution("propan", 3));
+        this.motherChainList.add(new motherchainsolution("but", 4));
+        this.motherChainList.add(new motherchainsolution("butan", 4));
+    }
+
+    /*
+     * SUFFIX, PREFIX 관련 함수 시작.
+     */
+
+    private void importSuffix() {
         this.suffixList.add("ol");
         this.suffixList.add("ane");
         this.suffixList.add("ene");
     }
-    private void importPrefix()
-    {
+
+    private void importPrefix() {
         this.prefixList.add("di");
         this.prefixList.add("tri");
     }
 
-    private int hasSuffix(String word)
-    {
-        
+    private String findSuffix(String word) {
         int suffixListSize = this.suffixList.size();
-        
-        for (int i=0;i<suffixListSize;i++)
-        {
-            if(word.equals(suffixList.get(i)))
-            {
-                return 2;
+        for (int i = 0; i < suffixListSize; i++) {
+            if (word.equals(suffixList.get(i))) {
+                return word;
             }
-        }//word가 suffix와 같다면 2 return
+        } // word가 suffix와 같다면 2 return
 
-        for (int i=0;i<suffixListSize;i++)
-        {
+        for (int i = 0; i < suffixListSize; i++) {
             int suffixSize = this.suffixList.get(i).length();
-            int wordSize   = word.length();
-            if(this.suffixList.get(i).equals(word.substring(wordSize-suffixSize,wordSize)))//마지막에 붙은게 같다면
+            int wordSize = word.length();
+            if (this.suffixList.get(i).equals(word.substring(wordSize - suffixSize, wordSize)))// 마지막에 붙은게 같다면
             {
-                return 1;
-            } //suffix 가 포함된 것이라면 return 1.
+                return this.suffixList.get(i);
+            } // suffix 가 포함된 것이라면 return 1.
         }
-        return -1; // 아니면 -1
+        return nosuffix; // 아니면 -1
     }
+
+    private String deleteSuffix(String word) {
+        String objectSuffix = this.findSuffix(word);
+        if (nosuffix.equals(objectSuffix)) 
+        {
+            return word;
+        } 
+        else 
+        {
+            return word.substring(0, word.length() - objectSuffix.length());
+        }
+    }
+
+    private int hasSuffix(String word) {
+        String findSuffixReturn = this.findSuffix(word);
+        if (findSuffixReturn.equals(nosuffix)) {
+            return -1;
+        } else if (findSuffixReturn.equals(word)) {
+            // word 자체가 suffix 인 경우
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+
+    /*
+     * SUFFIX, PREFIX 관련 함수 끝.
+     */
+
+
+    //모체 사슬의 길이를 알아내는 함수
+    private int findMotherNum(String motherWord)
+    {
+        String mother_without_suffix = this.deleteSuffix(motherWord);
+        
+        for (int i=0; i<this.motherChainList.size();i++)
+        {
+
+            //뭔가 좀 긴데, 결국은 어머니 체인 리스트 내의 원소를 차례로 검사하면서,
+            //suffix가 빠진 어머니 word 중 뒤에서 어머니 체인 내의 원소의 이름 만큼의 길이와
+            //어머니 체인 내의 원소의 이름 문자열이 동등한지 확인하는 작업.
+
+            //이를테면 butan-1-ol 에서 butan 은 4이고, 이는 List 의 Butan 원소에서 걸려서 4를 출력할 것.
+
+            String objectString = this.motherChainList.get(i).motherName;
+            if(mother_without_suffix.substring(mother_without_suffix.length()-objectString.length()+1).equals(objectString))
+            {
+                return this.motherChainList.get(i).number;    
+            }
+        }
+
+        return -1;
+    }
+
 
     private String hyphenMethod()
     {
@@ -69,9 +137,6 @@ public class refinetext {
                 //고로 2개 전에 모체사슬이 있다. 숫자 word가 있으므로.
 
                 return compoundWord[j-2];
-            }
-            else{
-                continue;
             }
         }
         //끝까지 suffix가 나오지 않았다.
@@ -109,13 +174,20 @@ public class refinetext {
 
     public void refinetextExecute() //대장함수이다. 차후 this.refinetextExecute 하나면 이 파일 전체가 실행된다.
     {
+        
+        String motherWord; //모체사슬이 포함된 하이픈 사이의 단어.
+        int motherNum; //모체사슬 길이.
+
+
+
         this.getName();
         elementmanager masterManager = new elementmanager();
         this.importSuffix();
         this.importPrefix();
+        this.importMotherChain();
 
-        String motherWord = this.hyphenMethod();
-
+        motherWord = this.hyphenMethod();
+        motherNum=this.findMotherNum(motherWord);
 
         
     }
